94f494adf325d15d6ce81aa290b532af48236df3
mojimoon <cryovit@outlook.com>
2023-08-24 17:25:27 +0800

    Data reorganization

--- src/components/Timetable.vue
+++ src/components/Timetable.vue
@@ -1,5 +1,4 @@
 <script setup>
-// generate 10 human-readable colors
 // https://stackoverflow.com/questions/10014271/generate-random-color-distinguishable-to-humans
 function selectColor(number) {
     const hue = number * 137.508; // use golden angle approximation
@@ -17,43 +16,128 @@ const day2number = {
     Unknown: 0
 };
 
-const char2day = {
-    M: 'Monday',
-    T: 'Tuesday',
-    W: 'Wednesday',
-    R: 'Thursday',
-    F: 'Friday',
-    S: 'Saturday',
-    U: 'Sunday',
-    X: 'Unknown'
+const number2char = {
+    1: 'M',
+    2: 'T',
+    3: 'W',
+    4: 'R',
+    5: 'F',
+    6: 'S',
+    7: 'U',
+    0: 'X'
 };
+
+import { ref, onMounted, watch } from 'vue';
+import courseDetail from '@/assets/courseDetail.json';
+
+const rawDetail = ref([]);
+const timetableList = ref([]);
+
+function updateTimetableList(courseCode) {
+    rawDetail.value = courseDetail[courseCode].sections;
+    // 需要的数据：
+    // rawDetail.sections.(any).activities
+    // .day -> 0~7
+    // .startTime -> 9:00~21:00
+    // .endTime -> 9:50~23:50
+    // .crn
+    // .section
+
+    for (const subsection of rawDetail.value) {
+        for (const activity of subsection.activities) {
+            console.log(activity);
+        }
+    }
+}
+
+function objectSpanMethod({ row, column, rowIndex, columnIndex }) {
+    if (columnIndex === 0) {
+        if (rowIndex % 2 === 0) {
+            return {
+                rowspan: 2,
+                colspan: 1
+            };
+        } else {
+            return {
+                rowspan: 0,
+                colspan: 0
+            };
+        }
+    }
+}
+
+function tableCellStyle (row) {
+    if (row.row[row.column.property].title !== undefined) {
+        return 'background-color:rgb(24 144 255 / 80%);color: #fff; border-radius:10px'
+    }
+}
+
+// 测试
+updateTimetableList("MA2185");
 </script>
 
 <template>
     <!-- 以表格形式呈现课程列表 -->
     <!-- Monday ~ Sunday, 09:00 - 23:00 -->
-    <div class="timetable">
-        <table>
-            <thead>
-                <tr>
-                    <th></th>
-                    <th v-for="day in 7" :key="day">{{ day }}</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="hour in 15" :key="hour">
-                    <td>{{ hour + 8 }}:00</td>
-                    <td v-for="day in 7" :key="day" :style="{ backgroundColor: selectColor(day2number[day] * hour) }">
-<!--                        <div v-for="course in timetable[day][hour]" :key="course.courseCode">-->
-<!--                            {{ course.courseCode }}-->
-<!--                        </div>-->
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+    <!-- 注意，时间点应该在行与行的交界处，而不是行的中间 -->
+    <!-- 如果暂时实现不了，可以显示 09 代替 09:00-09:50，小时数显示在正中间 -->
+    <div class="timetable-container">
+        <el-table :data="timetableList"
+                  :span-method="objectSpanMethod"
+                  border
+                  :header-cell-style="{background:'#d9e5fd', color:'black', fontWeight: 1000}"
+                  :cell-style="tableCellStyle"
+                    style="width: 100%">
+            <el-table-column prop="hour" label="Time" min-width="2"></el-table-column>
+            <el-table-column prop="M" label="Monday" min-width="4">
+                <template slot-scope="scope">
+                    <h4>{{scope.row.M.courseCode}}</h4>
+                    <div v-html="scope.row.M.info"></div>
+                </template>
+            </el-table-column>
+            <el-table-column prop="T" label="Tuesday" min-width="4">
+                <template slot-scope="scope">
+                    <h4>{{scope.row.T.courseCode}}</h4>
+                    <div v-html="scope.row.T.info"></div>
+                </template>
+            </el-table-column>
+            <el-table-column prop="W" label="Wednesday" min-width="4">
+                <template slot-scope="scope">
+                    <h4>{{scope.row.W.courseCode}}</h4>
+                    <div v-html="scope.row.W.info"></div>
+                </template>
+            </el-table-column>
+            <el-table-column prop="R" label="Thursday" min-width="4">
+                <template slot-scope="scope">
+                    <h4>{{scope.row.R.courseCode}}</h4>
+                    <div v-html="scope.row.R.info"></div>
+                </template>
+            </el-table-column>
+            <el-table-column prop="F" label="Friday" min-width="4">
+                <template slot-scope="scope">
+                    <h4>{{scope.row.F.courseCode}}</h4>
+                    <div v-html="scope.row.F.info"></div>
+                </template>
+            </el-table-column>
+            <el-table-column prop="S" label="Saturday" min-width="4">
+                <template slot-scope="scope">
+                    <h4>{{scope.row.S.courseCode}}</h4>
+                    <div v-html="scope.row.S.info"></div>
+                </template>
+            </el-table-column>
+            <el-table-column prop="U" label="Sunday" min-width="4">
+                <template slot-scope="scope">
+                    <h4>{{scope.row.U.courseCode}}</h4>
+                    <div v-html="scope.row.U.info"></div>
+                </template>
+            </el-table-column>
+        </el-table>
     </div>
 </template>
 
 <style scoped>
-
+.timetable-container {
+    width: 100%;
+    margin-top: 20px;
+}
 </style>
