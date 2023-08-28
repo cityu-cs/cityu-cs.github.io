558b628fa7cfeec375bf4990dd780fa93fe9badb
mojimoon <cryovit@outlook.com>
2023-08-28 16:56:59 +0800

    Timetable

--- src/components/CourseList.vue
+++ src/components/CourseList.vue
@@ -3,8 +3,9 @@ import { ref } from 'vue';
 import courses from '@/assets/newCourseList.json';
 import courseDetail from '@/assets/newCourseDetail.json';
 
+/* PART 1: 课程列表 */
+
 const fullList = ref(courses.course);
-const Detail = ref(courseDetail);
 
 const subjectList = ref(courses.subject.map(subject => {
     return {
@@ -38,7 +39,7 @@ const queryLevel = ref('');
 const resultList = ref([]);
 const currentList = ref([]);
 
-async function search() {
+function search() {
     const query = queryText.value.toLowerCase();
     const subject = querySubject.value;
     const level = queryLevel.value;
@@ -86,7 +87,7 @@ function updateCurrentList() {
     currentList.value = resultList.value.slice(start, end);
 }
 
-search();
+/* PART 2: 课程详情 */
 
 const tableItems = ref({});
 const tabs = ref([]);
@@ -113,6 +114,7 @@ function getDetail(courseCode) {
             activity.dayTime = char2Day[activity.day] + ' ' + activity.startTime + '-' + activity.endTime;
             activity.buildingRoom = activity.bldg + ' ' + activity.room;
             activity.webEnabled = activity.web ? 'Y' : 'N';
+            activity.courseCode = courseCode;
         }
     }
     // console.log(tableItems.value);
@@ -126,7 +128,89 @@ function handleCatalogClick(courseCode) {
     window.open(`http://www.cityu.edu.hk/catalogue/ug/current/course/${courseCode}.htm`, '_blank');
 }
 
-// getDetail("CS1302");
+/* PART 3: 课程表 */
+
+const hours = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22];
+const dayKeys = ['M', 'T', 'W', 'R', 'F', 'S', 'U'];
+const timetableList = ref([]);
+
+function saveLocalStorage() {
+    localStorage.setItem('timetableList', JSON.stringify(timetableList.value));
+}
+
+function loadLocalStorage() {
+    const data = localStorage.getItem('timetableList');
+    if (data) {
+        timetableList.value = JSON.parse(data);
+    }
+}
+
+function resetTimetable() {
+    if (!confirm('Reset Timetable?')) {
+        return;
+    }
+    localStorage.removeItem('timetableList');
+    // 强制刷新页面
+    window.location.reload();
+}
+
+function addToTimetable(row) {
+    row.startHour = parseInt(row.startTime.split(':')[0]);
+    row.endHour = parseInt(row.endTime.split(':')[0]);
+    timetableList.value.push(row);
+    saveLocalStorage();
+}
+
+function mergeTimetable({row, column, rowIndex, columnIndex}) {
+    if (columnIndex === 0) {
+        return {rowspan: 1, colspan: 1};
+    }
+    for (let act of timetableList.value) {
+        if (act.day === dayKeys[columnIndex - 1] && act.startHour === row) {
+            return {rowspan: act.endHour - act.startHour + 1, colspan: 1};
+        }
+    }
+    return {rowspan: 1, colspan: 1};
+}
+
+// https://stackoverflow.com/questions/10014271/generate-random-color-distinguishable-to-humans
+function selectColor(number) {
+    const hue = number * 137.508;
+    return `hsl(${hue},50%,60%)`;
+}
+
+function tableCellStyle ({row, column, rowIndex, columnIndex}) {
+    if (columnIndex === 0) {
+        return {'padding': '0', 'text-align': 'center'};
+    }
+    for (let act of timetableList.value) {
+        if (act.day === dayKeys[columnIndex - 1] && act.startHour === row) {
+            return {'background-color': selectColor(parseInt(act.courseCode.slice(2, 5))),
+                'color': '#fff', 'border-radius': '5px', 'padding': '0'};
+        } // 不同 courseCode 对应不同颜色
+    }
+    return {'padding': '0'};
+}
+
+function deleteFromTimetable(row, column, cell, event) {
+    // console.log(cell);
+    if (column.minWidth === 4) {
+        for (let i = 0; i < timetableList.value.length; i++) {
+            if (timetableList.value[i].day === column.rawColumnKey && timetableList.value[i].startHour === row) {
+                if (!confirm(`Delete ${timetableList.value[i].courseCode} - ${timetableList.value[i].section}?`)) {
+                    return;
+                }
+                timetableList.value.splice(i, 1);
+                saveLocalStorage();
+                break;
+            }
+        }
+    }
+}
+
+/* PART 4: 初始化 */
+search();
+loadLocalStorage();
 </script>
 
 <template>
@@ -144,7 +228,14 @@ function handleCatalogClick(courseCode) {
     </div>
 
     <div class="table-container">
-        <el-table :data="currentList" border stripe>
+        <el-table
+                :data="currentList"
+                border
+                stripe
+                :header-cell-style="{background:'#d9e5fd', color:'black', fontWeight: 1000}"
+                :row-style="{height: '0'}"
+                :cell-style="{padding: '0'}"
+        >
             <el-table-column class="table-medium" prop="courseCode" label="Course Code" min-width="2"></el-table-column>
             <el-table-column class="table-long" prop="courseTitle" label="Course Title" min-width="4"></el-table-column>
             <el-table-column class="table-short" prop="creditUnits" label="Credit Units" min-width="1"></el-table-column>
@@ -176,15 +267,29 @@ function handleCatalogClick(courseCode) {
     -->
     <div>
         <div class="tab-container">
-            <el-tabs value="activeTab" @tab-click="handleTabClick" :type="'card'">
-                <el-tab-pane v-for="item in tabs" :key="item" :label="item" :name="item"></el-tab-pane>
-            </el-tabs>
+            <div class="flex-container">
+                <div class="flex-item-left">
+                <el-tabs value="activeTab" @tab-click="handleTabClick" :type="'card'">
+                    <el-tab-pane v-for="item in tabs" :key="item" :label="item" :name="item"></el-tab-pane>
+                </el-tabs>
+                </div>
+                <div class="flex-item-right">
+                    <el-button type="primary" @click="resetTimetable">Reset Timetable</el-button>
+                </div>
+            </div>
             <p>Restrictions:</p>
             <ul>
                 <li v-for="item in restrictions[activeTab]" :key="item">{{item}}</li>
             </ul>
             <div class="table-container">
-                <el-table :data="tableItems[activeTab]" border stripe>
+                <el-table
+                        :data="tableItems[activeTab]"
+                        border
+                        stripe
+                        :header-cell-style="{background:'#d9e5fd', color:'black', fontWeight: 1000}"
+                        :row-style="{height: '0'}"
+                        :cell-style="{padding: '0'}"
+                >
                     <el-table-column class="table-short" prop="crn" label="CRN" min-width="1"></el-table-column>
                     <el-table-column class="table-short" prop="section" label="Section" min-width="1"></el-table-column>
                     <el-table-column class="table-medium" prop="dayTime" label="Time" min-width="2"></el-table-column>
@@ -203,6 +308,34 @@ function handleCatalogClick(courseCode) {
                 </el-table>
             </div>
         </div>
+    </div>
+
+
+    <div class="timetable-container">
+        <el-table
+                :data="hours"
+                border
+                :header-cell-style="{background:'#d9e5fd', color:'black', fontWeight: 1000, textAlign: 'center'}"
+                :span-method="mergeTimetable"
+                :row-style="{height: '70px'}"
+                :cell-style="tableCellStyle"
+                @cell-click="deleteFromTimetable"
+        >
+            <el-table-column label="Time" min-width="2">
+                <template #default="{row}">
+                    <p>{{row}}:00</p>
+                </template>
+            </el-table-column>
+            <el-table-column v-for="day in dayKeys" :key="day" :label="char2Day[day]" min-width="4">
+                <template #default="{row}">
+                    <div v-for="act in timetableList" :key="act.crn" v-show="act.day === day && act.startHour === row" class="timetable-data">
+                        <p class="timetable-data-title">{{act.courseCode}} - {{act.section}}</p>
+                        <p class="timetable-data-info">{{act.dayTime}}</p>
+                        <p class="timetable-data-info">{{act.buildingRoom}}</p>
+                    </div>
+                </template>
+            </el-table-column>
+        </el-table>
     </div>
 </template>
 
@@ -255,5 +388,29 @@ function handleCatalogClick(courseCode) {
 
 .table-container {
     margin-top: 10px;
+}
+
+.timetable-container {
+    width: 100%;
+    margin-top: 20px;
+}
+
+.timetable-data-title {
+    font-weight: bold;
+}
+
+.timetable-data-title, .timetable-data-info {
+    text-align: center;
+}
+
+.flex-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 10px;
+}
+
+.flex-item-right {
+    right: 0;
 }
 </style>
